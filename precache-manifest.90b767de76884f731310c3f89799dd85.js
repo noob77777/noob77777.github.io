69be2d4eeec7515405dd131a013ce2f2b3e4efff
noob77777 <abhishekpal7@outlook.com>
2020-06-14 19:52:01 +0530

    Deployment 0
    	deleted:    README.md
    	new file:   android-chrome-192x192.png
    	new file:   android-chrome-512x512.png
    	new file:   apple-touch-icon.png
    	new file:   asset-manifest.json
    	new file:   favicon-16x16.png
    	new file:   favicon-32x32.png
    	new file:   favicon.ico
    	new file:   index.html
    	new file:   manifest.json
    	deleted:    package-lock.json
    	deleted:    package.json
    	new file:   precache-manifest.90b767de76884f731310c3f89799dd85.js
    	deleted:    public/favicon.ico
    	deleted:    public/index.html
    	deleted:    public/logo192.png
    	deleted:    public/logo512.png
    	deleted:    public/manifest.json
    	renamed:    public/robots.txt -> robots.txt
    	new file:   service-worker.js
    	deleted:    src/App.css
    	deleted:    src/App.js
    	deleted:    src/App.test.js
    	deleted:    src/index.css
    	deleted:    src/index.js
    	deleted:    src/logo.svg
    	deleted:    src/serviceWorker.js
    	deleted:    src/setupTests.js
    	new file:   static/css/main.fd2f04d8.chunk.css
    	new file:   static/css/main.fd2f04d8.chunk.css.map
    	new file:   static/js/2.95b2c83d.chunk.js
    	new file:   static/js/2.95b2c83d.chunk.js.LICENSE.txt
    	new file:   static/js/2.95b2c83d.chunk.js.map
    	new file:   static/js/main.c4bcbdae.chunk.js
    	new file:   static/js/main.c4bcbdae.chunk.js.map
    	new file:   static/js/runtime-main.818df43a.js
    	new file:   static/js/runtime-main.818df43a.js.map

---
+++ precache-manifest.90b767de76884f731310c3f89799dd85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d0482fc0e4ce5ca5d2b794e8b91457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8137e937d2bf7d90933d",
+    "url": "/static/css/main.fd2f04d8.chunk.css"
+  },
+  {
+    "revision": "e8841b1e01d2d676a0dd",
+    "url": "/static/js/2.95b2c83d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95b2c83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8137e937d2bf7d90933d",
+    "url": "/static/js/main.c4bcbdae.chunk.js"
+  },
+  {
+    "revision": "b21301da78826aff9e3b",
+    "url": "/static/js/runtime-main.818df43a.js"
+  }
+]);
