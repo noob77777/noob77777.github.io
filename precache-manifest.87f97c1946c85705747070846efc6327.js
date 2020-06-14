63a38f4dcd9d7d3991583c55d9a3f37436fd3b55
noob77777 <abhishekpal7@outlook.com>
2020-06-14 20:10:55 +0530

    Updates

---
+++ precache-manifest.87f97c1946c85705747070846efc6327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4261167ff6b2eb3cc7bc969399239aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ac71053daabc066fb4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8d5975fd69b1bcdd647",
+    "url": "/static/js/2.4dbf7336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4dbf7336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ac71053daabc066fb4",
+    "url": "/static/js/main.cbac7ace.chunk.js"
+  },
+  {
+    "revision": "b21301da78826aff9e3b",
+    "url": "/static/js/runtime-main.818df43a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
