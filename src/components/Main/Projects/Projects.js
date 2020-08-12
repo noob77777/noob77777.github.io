9a9781660f12ae2187728f5caf701c49f8623f41
noob77777 <abhishekpal7@outlook.com>
2020-08-12 18:22:43 +0530

    Updates

--- src/components/Main/Projects/Projects.js
+++ src/components/Main/Projects/Projects.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react';
-import styles from './projects.module.scss';
+import React, { Component } from "react";
+import styles from "./projects.module.scss";
 
 class Projects extends Component {
     render() {
         return (
-            <div className={styles.Projects + ' Projects'}>
-                <div id='projects-main' className='container'>
+            <div className={styles.Projects + " Projects"}>
+                <div id="projects-main" className="container">
                     <br />
                     <br />
-                    <div className='row'>
-                        <div className={styles.projectsHeader + ' col s12'}>
+                    <div className="row">
+                        <div className={styles.projectsHeader + " col s12"}>
                             <h2>Projects</h2>
                         </div>
                     </div>
-                    <div className='row'>
-                        <div className='col s12 l5'>
-                            <div className={styles.card + ' card'}>
-                                <div className='card-image waves-effect waves-block waves-light'>
+                    <div className="row">
+                        <div className="col s12 l5">
+                            <div className={styles.card + " card"}>
+                                <div className="card-image waves-effect waves-block waves-light">
                                     <img
-                                        className='activator'
-                                        src={require('../../../img/firebird.png')}
-                                        alt=''
+                                        className="activator"
+                                        src={require("../../../img/firebird.png")}
+                                        alt=""
                                     />
                                 </div>
-                                <div className='card-content'>
-                                    <span className='card-title activator'>
+                                <div className="card-content">
+                                    <span className="card-title activator">
                                         Firebird
-                                        <i className='material-icons right'>
+                                        <i className="material-icons right">
                                             arrow_drop_up
                                         </i>
                                     </span>
                                     <p>Real time messenger</p>
                                 </div>
-                                <div className='card-action'>
+                                <div className="card-action">
                                     <a
-                                        href='https://noob77777.github.io/firebird.github.io'
-                                        target='_blank'
-                                        rel='noopener noreferrer'
+                                        href="https://noob77777.github.io/firebird.github.io"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
                                         id={styles.link}
                                     >
                                         Link
                                     </a>
                                 </div>
-                                <div className='card-reveal'>
-                                    <span className='card-title'>
+                                <div className="card-reveal">
+                                    <span className="card-title">
                                         FireBird
-                                        <i className='material-icons right'>
+                                        <i className="material-icons right">
                                             close
                                         </i>
                                     </span>
                                     <p>
                                         Created using Bootstrap and Firebase
-                                        real-time database.{' '}
+                                        real-time database.{" "}
                                     </p>
                                     <p>
                                         Major changes in progress. Shifting to
@@ -67,37 +67,62 @@ class Projects extends Component {
                             <br />
                             <br />
                         </div>
-                        <div className='col s12 l5 offset-l2'>
-                            <div className={styles.card + ' card'}>
-                                <div className='card-image waves-effect waves-block waves-light'>
+                        <div className="col s12 l5 offset-l2">
+                            <div className={styles.card + " card"}>
+                                <div className="card-image waves-effect waves-block waves-light">
                                     <img
-                                        className='activator'
-                                        src={require('../../../img/project2.jpg')}
-                                        alt=''
+                                        className="activator"
+                                        src={require("../../../img/project2.jpg")}
+                                        alt=""
                                     />
                                 </div>
-                                <div className='card-content'>
-                                    <span className='card-title activator '>
+                                <div className="card-content">
+                                    <span className="card-title activator ">
                                         Deep Learning in C++
-                                        <i className='material-icons right'>
+                                        <i className="material-icons right">
                                             arrow_drop_up
                                         </i>
                                     </span>
-                                    <p>Work in progress...</p>
+                                    <p>
+                                        Fast neural networks implementation in
+                                        C++
+                                    </p>
                                 </div>
-                                <div className='card-action'>
-                                    <a href='/#projects-main' id={styles.link}>
+                                <div className="card-action">
+                                    <a
+                                        href="https://github.com/noob77777/deep-learning/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        id={styles.link}
+                                    >
                                         Link
                                     </a>
                                 </div>
-                                <div className='card-reveal'>
-                                    <span className='card-title'>
+                                <div className="card-reveal">
+                                    <span className="card-title">
                                         Deep Learning in C++
-                                        <i className='material-icons right'>
+                                        <i className="material-icons right">
                                             close
                                         </i>
                                     </span>
-                                    <p></p>
+                                    <p>
+                                        Fast neural networks implementation in
+                                        C++ using x86-64 SIMD instructions.
+                                        <br />
+                                        Fast optimization using Adam: ADAptive
+                                        Moment estimation.
+                                    </p>
+                                    <p>
+                                        Supports custom gradients, loss
+                                        functions and activations.
+                                    </p>
+                                    <p>
+                                        Serializable objects to save trained
+                                        models.
+                                    </p>
+                                    <p>
+                                        Checkout the github repository.
+                                    </p>
                                 </div>
                             </div>
                             <br />
